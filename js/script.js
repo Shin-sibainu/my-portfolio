94f7b7191fc14d668da1656f4ce317830766dee0
Shin-sibainu <shincode0712@gmail.com>
2021-09-15 09:54:33 +0900

    slides, about section done. but skills section cant done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
-    $('#slides').superslides({
-        animation: "fade",
-        play: 4000,
-        pagination: false
-    })
-})
+$(document).ready(function () {
+  $("#slides").superslides({
+    animation: "fade",
+    play: 4000,
+    pagination: false,
+  });
 
+  var typed = new Typed(".typed", {
+    strings: ["System Engineer.", "Web Developer.", "Game Dveloper."],
+    typeSpeed: 100,
+    loop: true,
+  });
+});
