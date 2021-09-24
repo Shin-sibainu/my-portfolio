f2fc38bed6f2a27505a36783de1aa6db7a822c1c
Shin-sibainu <shincode0712@gmail.com>
2021-09-24 15:49:23 +0900

    sticky navber added. but responsive a right little hamidasu

--- js/script.js
+++ js/script.js
@@ -89,4 +89,23 @@ $(document).ready(function () {
     });
     return false;
   });
+
+  /* fixedNav */
+  const nav = $("#navigation");
+  // console.log(nav);
+  const navTop = nav.offset().top;
+  console.log(navTop); //880
+
+  $(window).on("scroll", stickyNavigation);
+
+  function stickyNavigation() {
+    var body = $("body");
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
 });
